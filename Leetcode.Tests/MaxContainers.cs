ff762ac47b0bf804473e1afab3e6bd586f5565ae
Joseph Baruch <josephbaruch48@gmail.com>
2026-06-19 10:36:38 +0200

    setting up MaxContainers

---
+++ Leetcode.Tests/MaxContainers.cs
@@ -0,0 +1,19 @@
+// Run: dotnet test --filter "FullyQualifiedName=Leetcode.Tests.MaxContainersTestsLoop.MaxContainers_ReturnsExpected"
+namespace Leetcode.Tests;
+
+public class MaxContainersTestsLoop
+{
+    public static IEnumerable<object[]> Cases => new[]
+    {
+        new object[] { 2, 3, 15, 4 },
+        // new object[] { 3, 5, 20, 4 },
+    };
+
+    [Theory]
+    [MemberData(nameof(Cases))]
+    public void MaxContainers_ReturnsExpected(int n, int w, int maxWeight, int expected)
+    {
+        var s = new MaxContainersClass();
+        Assert.Equal(expected, s.MaxContainers(n, w, maxWeight));
+    }
+}
---
+++ Leetcode/MaxContainers.cs
@@ -0,0 +1,15 @@
+// 3492. Maximum Containers on a Ship
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class MaxContainersClass {
+    private int MaxContainersOne(int n, int w, int maxWeight)
+    {
+        return 0;
+    }
+
+    public int MaxContainers(int n, int w, int maxWeight) {
+        return MaxContainersOne(n, w, maxWeight);
+    }
+}
